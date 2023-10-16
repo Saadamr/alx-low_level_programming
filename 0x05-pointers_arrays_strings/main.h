d1e1f776e3ba4b1ef783bd5c8c220d83fe44a1bd
Saad <s.amroui@mundiapolis.ma>
2023-10-16 16:35:00 -0700

    6

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,6 +7,8 @@ void print_rev(char *s);
 int _putchar(char c);
 void rev_string(char *s);
 int _putchar(char c);
+void puts2(char *str);
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 #endif /* MAIN_H */
