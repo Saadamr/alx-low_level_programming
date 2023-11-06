9103159ff0ca51e2c48241b27312d36cc6399c9f
Saadamr <s.amroui@mundiapolis.ma>
2023-11-06 13:05:45 -0800

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,15 @@
-#ifndef DOG_H 
+#ifndef DOG_H
 #define DOG_H
 
-/**
-* struct dog - Define a dog structure
-* @name: Name of the dog (string)
-* @age: Age of the dog (float)
-* @owner: Owner of the dog (string)
-*/
-struct dog
-{
-char *name;
-float age;
-char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+typedef struct dog {
+    char *name;
+    float age;
+    char *owner;
+} dog_t;  /* Define dog_t as an alias for struct dog. */
+
+void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
