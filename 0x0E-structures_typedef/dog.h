081beda21f2faa55386dd1b20124e6a3da3a635c
Saadamr <s.amroui@mundiapolis.ma>
2023-11-06 12:59:49 -0800

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H 
+#define DOG_H
+
+/**
+* struct dog - Define a dog structure
+* @name: Name of the dog (string)
+* @age: Age of the dog (float)
+* @owner: Owner of the dog (string)
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /* DOG_H */
